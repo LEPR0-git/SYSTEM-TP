45fd7c6744a4df6745c552ec3ee577a6fae8e621
BassilekinJean <jean.bassilekin@facsciences-uy1.cm>
2026-06-19 21:21:19 +0100

    feat: llist.c

--- libsysutil/include/llist.h
+++ libsysutil/include/llist.h
@@ -1,22 +1,36 @@
 #ifndef LLIST_H
 #define LLIST_H
+
 #include <stddef.h>
 
-typedef struct  LLNode
-{
+typedef struct LLNode {
     void *data;
-    struct LLNode * next;
-}LLNode;
+    struct LLNode *next;
+} LLNode;
 
-typedef struct 
-{
-    /* data */
-};
+typedef struct {
+    LLNode *head;
+    LLNode *tail;
+    size_t length;
+    void (*free_data)(void *);
+} LList;
 
+/* callback de libération (peut être NULL) */
+LList *ll_create(void (*free_data)(void *));
 
+int ll_push_back(LList *l, void *data);
 
+int ll_push_front(LList *l, void *data);
 
+void *ll_pop_front(LList *l);
 
+void *ll_get(const LList *l, size_t index);
+
+int ll_remove(LList *l, void *data, int (*cmp)(const void *, const void *));
+
+void ll_foreach(const LList *l, void (*fn)(void *data, size_t idx));
+
+void ll_destroy(LList **l);
 
 
 #endif
