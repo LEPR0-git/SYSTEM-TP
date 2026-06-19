f98eaa65fbe6f478e6617da585f122911ebf6170
testeurx237-beep <testeurX237@gmail.com>
2026-06-19 21:24:07 +0100

    first commit

--- libsysutil/include/llist.h
+++ libsysutil/include/llist.h
@@ -10,9 +10,19 @@ typedef struct  LLNode
 
 typedef struct 
 {
-    /* data */
-};
+    LLNode *head;
+    LLNode *tail;
+    size_t length;
+    void (*free_data)(void*);  // callback de liberation peut etre NULL
 
+}LList;
+
+LList *ll_create(void(*free_data)(void*));
+int ll_push_back(LList *l, void* data);
+int ll_push_front(LList *l,void* data);
+void *ll_pop(LList *l);
+void *ll_get(const LList *l, size_t index);
+int ll_remove(LList *l, void *data, int)
 
 
 
---
+++ libsysutil/src/llist.c
@@ -0,0 +1,1 @@
+#ifndef LLIST_H
